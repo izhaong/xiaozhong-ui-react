45683704e28234204f30ab6eb10c2a55187a01bb
izhaong <164165005@qq.com>
2021-07-10 11:17:03 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 /*
- * @Description: 
+ * @Description:
  * @Author: 仲灏<izhaong@outlook.com>
  * @Date: 2021-07-09 11:19:53
  * @LastEditors: 仲灏<izhaong@outlook.com>
- * @LastEditTime: 2021-07-09 18:14:38
+ * @LastEditTime: 2021-07-10 11:16:51
  */
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  
   return (
     <div className="App">
       <header className="App-header">
-          <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.test
         </p>
         <a
           className="App-link"
